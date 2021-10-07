aeb7f4950f251dcf4a2fb7a49ffa4c8da6e0e67a
Zachary Crawford <zrcrawford15@gmail.com>
2021-10-06 23:03:32 -0700

    Added notes and getters and setters

--- nhl.py
+++ nhl.py
@@ -2,32 +2,39 @@ import requests
 from team import *
 
 
-def get_team_id(name, res):
-    '''
+def get_team_id(name, resp):
+    """
     Gets the team ID from the name
     :param name: Name of team
-    :param res: JSON response from the NHL api
+    :param resp: JSON response from the NHL api
     :return: int ID
-    '''
+    """
 
-    for key in res['teams']:
+    for key in resp['teams']:
         if key['name'] == name:
             print(f"Key value is {key['id']}")
             return key['id']
 
 def get_team_object(ID, year):
-    '''
+    """
     Gets the team stats based on the ID
     :param ID: int Team ID
     :param year: str year in the form YYYYYYYY e.g (20102011)
     :return: JSON stats for given team and year
-    '''
+    """
     res = requests.get(f"https://statsapi.web.nhl.com/api/v1/teams/{ID}?expand=team.stats&season={year}")
     team = res.json()
     return get_team_stats(team)
 
-def get_team_stats(res):
-    return res['teams'][0]['teamStats'][0]['splits'][0]['stat']
+
+def get_team_stats(resp):
+    """
+    Gets the season stats for a team from the JSON object
+    :param resp: team JSON object
+    :return:
+    """
+    return resp['teams'][0]['teamStats'][0]['splits'][0]['stat']
+
 
 if __name__ == "__main__":
 
@@ -35,6 +42,11 @@ if __name__ == "__main__":
     response = requests.get(URL)
     res = response.json()
 
+    year = "1995-1996"                              # TODO: Get this from the GUI and covert it to 'YYYYYYYY'
+
+    # Remove the - from the input or just make value of input = YYYYYYYY without -
+    test = year.replace('-', '')
+    print(test)
 
     team_1_name = "Colorado Avalanche"             # TODO: Get this from the GUI with the year
     team_1_ID = get_team_id(team_1_name, res)
@@ -45,4 +57,4 @@ if __name__ == "__main__":
 
     # add stats to team object
     team_1.wins = team_1_object['wins']
-    print(team_1.wins)
+    # print(f"{team_1.name} won {team_1.wins} games in the year {year}")
--- team.py
+++ team.py
@@ -1,18 +1,89 @@
 class Team:
     def __init__(self, name, ID):
-        self.name = name
-        self.ID = ID
-        self.wins = None
-        self.losses = None
-        self.OT = None
-        self.pts = None
-        self.goal_per_game = None
-        self.goals_against_per_game = None
-        self.power_play_percentage = None
-        self.penalty_kill_percentage = None
-        self.shots_per_game = None
-        self.shots_allowed_per_game = None
-        self.save_percentage = None
+        self._name = name
+        self._ID = ID
+        self._wins = None
+        self._losses = None
+        self._OT = None
+        self._pts = None
+        self._goal_per_game = None
+        self._goals_against_per_game = None
+        self._power_play_percentage = None
+        self._penalty_kill_percentage = None
+        self._shots_per_game = None
+        self._shots_allowed_per_game = None
+        self._save_percentage = None
 
-        print(f"New team created named {self.name} with ID of {self.ID}")
+        print(f"New team created named {self._name} with ID of {self._ID}")
 
+    def get_id(self):
+        return self._ID
+
+    def get_name(self):
+        return self._name
+
+    def get_wins(self):
+        return self._wins
+
+    def set_wins(self, wins):
+        self._wins = wins
+
+    def get_losses(self):
+        return self._losses
+
+    def set_losses(self, losses):
+        self._losses = losses
+
+    def get_ot(self):
+        return self._OT
+
+    def set_ot(self, OT):
+        self._OT = OT
+
+    def get_pts(self):
+        return self._pts
+
+    def set_pts(self, pts):
+        self._pts = pts
+
+    def get_goal_per_game(self):
+        return self._goal_per_game
+
+    def set_goal_per_game(self, goals_per_game):
+        self._goal_per_game = goals_per_game
+
+    def get_goals_against_per_game(self):
+        return self._goals_against_per_game
+
+    def set_goals_against_per_game(self, goals_against_per_game):
+        self._goals_against_per_game = goals_against_per_game
+
+    def get_power_play_percentage(self):
+        return self._power_play_percentage
+
+    def set_power_play_percentage(self, set_power_play_percentage):
+        self._power_play_percentage = set_power_play_percentage
+
+    def get_penalty_kill_percentage(self):
+        return self._penalty_kill_percentage
+
+    def set_penalty_kill_percentage(self, penalty_kill_percentage):
+        self._penalty_kill_percentage = penalty_kill_percentage
+
+    def get_shots_per_game(self):
+        return self._shots_per_game
+
+    def set_shots_per_game(self, shots_per_game):
+        self._shots_per_game = shots_per_game
+
+    def get_shots_allowed_per_game(self):
+        return self._shots_allowed_per_game
+
+    def set_shots_allowed_per_game(self, shots_allowed_per_game):
+        self._shots_allowed_per_game = shots_allowed_per_game
+
+    def get_save_percentage(self):
+        return self._save_percentage
+
+    def set_save_percentage(self, save_percentage):
+        self._save_percentage = save_percentage
